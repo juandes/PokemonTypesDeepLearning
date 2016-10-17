5d84cbf6231c053e3418eb8e8e964d75a3bc5441
Juan De Dios Santos <juan.d.diossantos@gmail.com>
2016-10-17 21:03:20 +0200

    Updated main

--- main.py
+++ main.py
@@ -17,21 +17,25 @@ import pandas as pd
 IMAGE_SIZE = 32
 
 
-def load_images2():
+def load_images():
     """
     Returns a tuple made of an array of 18 (types) datasets of the shape 
-    (len(type_image), IMAGE_SIZE, IMAGE_SIZE, 3) and another array that has 
-    the labels (Pokemon type)
+    (len(type_image), IMAGE_SIZE, IMAGE_SIZE, 3), another array that has 
+    the labels (Pokemon type), and an array made of the name of the Pokemon
     """
     labels = []
+    pokemon_name = []
     image_index = 0
     # 714 because the Flying Pokemon were removed
     dataset = np.ndarray(shape=(714, IMAGE_SIZE, IMAGE_SIZE, 3),
                             dtype=np.float32)
+    # Loop through all the types directories
     for type in os.listdir('./Images/data'):
         type_images = os.listdir('./Images/data/' + type + '/')
+        # Loop through all the images of a type directory
         for image in type_images:
-            image_file = os.path.join(os.getcwd(), 'Images/Resized', type, image)
+            image_file = os.path.join(os.getcwd(), 'Images/data', type, image)
+            pokemon_name.append(image)
             # reading the images as they are; no normalization, no color editing
             image_data = (ndimage.imread(image_file, mode='RGB'))
             if image_data.shape != (IMAGE_SIZE, IMAGE_SIZE, 3):
@@ -43,35 +47,7 @@ def load_images2():
             labels.append(type)
             #dataset_names.append(dataset.astype(int))
         
-    return (dataset, labels)
-
-
-def load_images(image_size):
-    """
-    Returns a tuple made of an array of 18 (types) datasets of the shape 
-    (len(type_image), IMAGE_SIZE, IMAGE_SIZE, 3) and another array that has 
-    the labels (Pokemon type)
-    """
-    dataset_names = []
-    labels = []
-    for type in os.listdir('./Images/Resized'):
-        image_index = 0
-        type_images = os.listdir('./Images/Resized/' + type + '/')
-        dataset = np.ndarray(shape=(len(type_images), IMAGE_SIZE, IMAGE_SIZE, 3),
-                            dtype=np.float32)
-        labels.append(type)
-        for image in type_images:
-            image_file = os.path.join(os.getcwd(), 'Images/Resized', type, image)
-            # reading the images as they are; no normalization, no color editing
-            image_data = (ndimage.imread(image_file, mode='RGB'))
-            if image_data.shape != (IMAGE_SIZE, IMAGE_SIZE, 3):
-                raise Exception('Unexpected image shape: %s %s' % (str(image_data.shape), image_file))
-            dataset[image_index, :, :] = image_data
-            image_index += 1
-            #dataset_names.append(dataset.astype(int))
-        dataset_names.append(dataset.astype(int))
-        
-    return (dataset_names, labels)
+    return (dataset, labels, pokemon_name)
 
 
 def first_image_per_type(dataset):
@@ -96,15 +72,24 @@ def conv_model(X, y):
   with tf.variable_scope('conv_layer1'):
     h_conv1 = learn.ops.conv2d(X, n_filters=32, filter_shape=[5, 5],
                                bias=True, activation=tf.nn.relu)
+
+    # Print the shape of the tensor
+    print ("Convolution layer 1 shape: {}".format(h_conv1.get_shape()))
     h_pool1 = max_pool_2x2(h_conv1)
+    print ("Convolution layer 1 shape (after pooling): {}".format(h_conv1.get_shape()))
+
   # second conv layer will compute 64 features for each 5x5 patch.
   with tf.variable_scope('conv_layer2'):
     h_conv2 = learn.ops.conv2d(h_pool1, n_filters=64, filter_shape=[5, 5],
                                bias=True, activation=tf.nn.relu)
+    print ("Convolution layer 2 shape: {}".format(h_conv2.get_shape()))
+
     h_pool2 = max_pool_2x2(h_conv2)
+    print ("Convolution layer 2 shape (after pooling): {}".format(h_pool2.get_shape()))
 
     # reshape tensor into a batch of vectors
     h_pool2_flat = tf.reshape(h_pool2, [-1, 8 * 8 * 64])
+    print ("Final shape: {}", h_pool2_flat.get_shape())
   # densely connected layer with 1024 neurons.
   h_fc1 = learn.ops.dnn(
       h_pool2_flat, [1024], activation=tf.nn.relu, dropout=0.5)
@@ -112,7 +97,8 @@ def conv_model(X, y):
 
 
 def main():
-    images, labels = load_images2()
+    images, labels, pokemon = load_images()
+    pokemon_test = []
     print (labels)
 
     # Label encoder
@@ -122,9 +108,7 @@ def main():
     transformed_labels = le.transform(labels)
     print (transformed_labels)
 
-
-    df = pd.DataFrame(np.random.randn(714, 2))
-    msk = np.random.rand(len(df)) < 0.8
+    msk = np.random.rand(714) < 0.8
     print (msk)
     true_indexes = []
     false_indexes = []
@@ -137,6 +121,7 @@ def main():
         else:
             false_indexes.append(idx)
             test_labels.append(transformed_labels[idx])
+            pokemon_test.append(pokemon[idx])
 
     print(len(true_indexes))
     training_set = np.delete(images, false_indexes, 0)
@@ -151,6 +136,7 @@ def main():
     plt.imshow(test_set[100])
     plt.show()
     print(test_labels[100])
+    print(pokemon_test[100])
 
 
 
@@ -181,9 +167,9 @@ def main():
     # Training and predicting.
     classifier = learn.TensorFlowEstimator(
         model_fn=conv_model, n_classes=17, batch_size=100, steps=20000,
-        learning_rate=0.001)
-    classifier.fit(reshaped_dataset, training_labels, logdir=os.getcwd() + 'model_20000a_logs')
-    classifier.save(os.getcwd() + '/model_20000a')
+        learning_rate=0.001, verbose=2)
+    classifier.fit(reshaped_dataset, training_labels, logdir=os.getcwd() + 'model_20000b_logs')
+    classifier.save(os.getcwd() + '/model_20000b')
     score = metrics.accuracy_score(
         test_labels, classifier.predict(reshaped_testset))
     print('Accuracy: {0:f}'.format(score))
@@ -193,6 +179,10 @@ def main():
         'Ground' 'Ice' 'Normal' 'Poison' 'Psychic' 'Rock' 'Steel' 'Water']
 
     print (metrics.classification_report(test_labels, prediction_labels))
+    print (test_labels)
+    print (prediction_labels)
+    print (pokemon_test)
+
     
 
     f1 = metrics.f1_score(
